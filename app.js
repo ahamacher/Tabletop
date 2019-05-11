fdf740cc00de49ce310619451420176993bce06d
Andrew <andrew.hamacher@gmail.com>
2019-05-11 15:28:18 -0700

    Initiate user auth and app.js entry file

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
+const bodyParser = require("body-parser");
+const db = require("./config/keys").mongoURI;
+const passport = require('passport');
+require('./config/passport')(passport);
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log("Connected to MonoDb sucessfully"))
+  .catch(err => console.log(err));
+
+app.use(bodyParser.json());
+
+app.use(passport.initialize());
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
+
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server is running on poert ${port}`));
+

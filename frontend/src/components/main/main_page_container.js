e72cd6e9c24d78c71b7727f202468dd8434256e3
Jason Yu <jason@jasonyu.co>
2019-05-14 10:54:56 -0700

    Add styling for session forms

---
+++ frontend/src/components/main/main_page_container.js
@@ -0,0 +1,8 @@
+import { connect } from 'react-redux';
+import MainPage from './main_page';
+
+const mapStateToProps = state => ({
+    loggedIn: state.session.isAuthenticated
+});
+
+export default connect(mapStateToProps, null)(MainPage);

9b5f223f7f4c9c5d9140529591b0d41824a7b4fe
Kevin Cheang <cheangk009@gmail.com>
2019-05-13 23:49:58 -0700

    add image reducer, actions, and api_util. Also add a temp image index

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -12,6 +12,8 @@ import SignupFormContainer from './session/signup_form_container';
 import NavBarContainer from './nav/navbar_container';
 import GamePage from './game/game_page.js';
 import MessagesPage from "./messages/messages_page";
+import ImagesContainer from './images/images_container';
+
 
 
 const App = () => (
@@ -23,6 +25,7 @@ const App = () => (
       <AuthRoute exact path="/signup" component={SignupFormContainer} />
       <ProtectedRoute exact path="/games/:gameId" component={GamePage} />
       <ProtectedRoute exact path="/messages" component={MessagesPage} />
+      <ProtectedRoute exact path="/images" component={ImagesContainer} />
     </Switch>
   </div>
 );
--- frontend/src/reducers/entities_reducer.js
+++ frontend/src/reducers/entities_reducer.js
@@ -1,6 +1,9 @@
 import { combineReducers } from 'redux';
-import items from './items_reducer'
+import items from './items_reducer';
+import images from './images_reducer';
+
 
 export default combineReducers({
-    items
+    items,
+    images
 });

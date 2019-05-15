3bc3748de8f61903b7a87c8c999d42e483226c56
Jathrone <jason.li.feed@gmail.com>
2019-05-15 10:43:05 -0700

    add debugger to test heroku

--- app.js
+++ app.js
@@ -23,6 +23,10 @@ if (process.env.NODE_ENV === 'production') {
   app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
   })
+
+  const port = process.env.PORT || 5000;
+  
+  app.listen(port, () => console.log(`Server is running on port ${port}`));
 }
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -59,7 +63,6 @@ app.use("/api/messages", messages);
 app.use("/api/images", images);
 app.use("/api/games", games);
 app.use("/api/image_instances", imageInstances);
-
 
 const port = process.env.PORT || 5000;
 

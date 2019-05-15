b29f0e4398a706731d30a6df5fd8915e12f75c65
Jathrone <jason.li.feed@gmail.com>
2019-05-15 10:01:52 -0700

    configure for heroku

---
+++ config/keys_prod.js
@@ -0,0 +1,7 @@
+module.exports = {
+    mongoURI: process.env.MONGO_URI,
+    secretOrKey: process.env.SECRET_OR_KEY,
+    s3secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
+    s3accessKeyId: process.env.S3_ACCESS_KEY_ID,
+    s3region: process.env.S3_REGION
+}

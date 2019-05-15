b379a9ed988a0e9f6ea066a637e4b527872305f3
Kevin Cheang <cheangk009@gmail.com>
2019-05-15 10:10:17 -0700

    fixed order of messages

--- frontend/src/reducers/messages_reducer.js
+++ frontend/src/reducers/messages_reducer.js
@@ -10,20 +10,16 @@ const messagesReducer = (state = {}, action) => {
         case RECEIVE_MESSAGES:
     
             while (action.messages.length > 5) {
-               action.messages.pop();
+                action.messages.pop();
             }
-            
-            newState = merge({}, keyBy(action.messages, "_id"));
+            let reversed = action.messages.reverse();
+
+            newState = merge({}, keyBy(reversed, "_id"));
             return newState
 
         case RECEIVE_MESSAGE:
             
-            let newerMessages;
-            if (Object.values(state).length >= 5) {
-                const dates = keyBy(Object.values(state), "date");
-                newerMessages = Object.values(state).filter( message => message.date !== Object.keys(dates).sort()[0]);
-            }
-            newState = merge({}, keyBy(newerMessages, "_id"), {[action.message._id]: action.message} )
+            newState = merge({}, state, {[action.message._id]: action.message} )
             return newState;
         default:
             return state;

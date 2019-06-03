5eed920e7d5e7840ad7c1139355f8e49eb1df274
Andrew <andrew.hamacher@gmail.com>
2019-06-03 11:40:11 -0700

    heroku upload test

--- app.js
+++ app.js
@@ -24,9 +24,9 @@ if (process.env.NODE_ENV === 'production') {
     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
   });
 }
-const port = process.env.PORT || 5000;
+// const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`Server is running on port ${port}`));
+// app.listen(port, () => console.log(`Server is running on port ${port}`));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());

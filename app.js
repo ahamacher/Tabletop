d24d4e8600d31940afa1aeeef0be6cb5e471893d
Jason Yu <jason@jasonyu.co>
2019-06-03 12:02:49 -0700

    Add condition for development mode

--- app.js
+++ app.js
@@ -23,10 +23,10 @@ if (process.env.NODE_ENV === 'production') {
   app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
   });
+} else {
+  const port = process.env.PORT || 5000;
+  app.listen(port, () => console.log(`Server is running on port ${port}`));
 }
-// const port = process.env.PORT || 5000;
-
-// app.listen(port, () => console.log(`Server is running on port ${port}`));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
